9b9d3f600bf405751c17a3944c934507531041df
Danny2706 <danikume27@gmail.com>
2026-03-08 20:15:35 +0300

    deployed Endpoint Update

--- src/app/api/contactApi.ts
+++ src/app/api/contactApi.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/contact";
+const API_URL = "https://gym-lead-latest-2.onrender.com/api/contact";
 
 export const sendContactForm = (data: any) => {
   return axios.post(API_URL, data);
--- src/app/service/axiosInstance.ts
+++ src/app/service/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://gym-lead-latest-2.onrender.com/api",
 });
 
 // Attach token automatically
